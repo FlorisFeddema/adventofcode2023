723d3473bafb44ec4267bf44268edad539d26f13
Floris Feddema <Floris1996@hotmail.com>
2023-12-05 12:55:57 +0100

    add part 1 day 4

---
+++ day5/part1/run.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"adventofcode2023/helper"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	result := 2147483647
+	inputData := helper.ReadFileToStringArray("input.txt")
+
+	start := time.Now()
+
+	seeds := strings.Split(strings.TrimPrefix(inputData[0], "seeds: "), " ")
+
+	mappings := [][]string{}
+	mappings = append(mappings, []string{})
+
+	mappingCount := 0
+	for i := 3; i < len(inputData); i++ {
+		if inputData[i] == "" {
+			i++
+			mappingCount++
+			mappings = append(mappings, []string{})
+			continue
+		}
+		mappings[mappingCount] = append(mappings[mappingCount], inputData[i])
+	}
+
+	for _, seed := range seeds {
+		value := helper.StringToInt(seed)
+		for _, mapping := range mappings {
+			for _, typeRange := range mapping {
+				rangeValues := strings.Split(typeRange, " ")
+				destinationRange := helper.StringToInt(rangeValues[0])
+				sourceRange := helper.StringToInt(rangeValues[1])
+				rangeLength := helper.StringToInt(rangeValues[2])
+				if value >= sourceRange && value < sourceRange+rangeLength {
+					value = destinationRange - sourceRange + value
+					break
+				}
+			}
+		}
+		if value < result {
+			result = value
+		}
+	}
+
+	elapsed := time.Since(start)
+
+	fmt.Printf("Result: %[1]d\n", result)
+	fmt.Printf("Time: %s\n", elapsed)
+}
+
+// 79 81 81 81 74 78 78 82
